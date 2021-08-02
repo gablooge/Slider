ba1009518bd8c13adc35cef456b5e9317fc13dcf
Samsul Hadi <samsulhadikediri@gmail.com>
2021-08-02 10:33:50 +0700

    Initial Commit

---
+++ Slider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slider
+//
+//  Created by Samsul Hadi on 02/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Samsul Hadi on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
